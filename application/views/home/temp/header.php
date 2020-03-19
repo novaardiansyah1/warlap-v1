c4538a3c7c43a8c180f2052c73db19a80b17f9c7
novaardiansyah78 <novaardiansyah817@gmail.com>
2020-03-19 19:30:27 +0700

    menambah link pada navbar

--- application/views/home/temp/header.php
+++ application/views/home/temp/header.php
@@ -5,7 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="<?= base_url('assets/dist/css/adminlte.min.css?v='.time()) ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/dist/css/adminlte.min.css?v='.time()) ?>">
   <link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome-free/css/all.min.css?v='.time()) ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/owner/home/css/index.css?v='.time()) ?>">
   <title><?= $title ?></title>
 </head>
 <body>
--- application/views/home/temp/navbar.php
+++ application/views/home/temp/navbar.php
@@ -1,6 +1,7 @@
 <section id="navbar">
   
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" 
   data-toggle="collapse" data-target="#navbarNavAltMarkup" 
@@ -10,10 +11,40 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link active" href="#">Home</a>
+      <a class="nav-item nav-link" href="#about">Tentang</a>
+      <a class="nav-item nav-link" href="#fitur">Fitur</a>
+      <a class="nav-item nav-link" href="#total">Total</a>
+      <a class="nav-item nav-link" href="#news">Terkini</a>
+      <a class="nav-item nav-link" href="#lapor">Lapor</a>
+      <?php if($this->session->userdata('username')) : ?>
+        <a class="nav-item nav-link" href="<?= site_url('profile') ?>">
+          <i class="fas fa-user"></i> Profile
+        </a>
+        <a class="nav-item nav-link" href="<?= site_url('auth/logout') ?>">
+          <i class="fas fa-sign-out-alt"></i> Logout
+        </a>
+      <?php else : ?>
+        <a class="nav-item nav-link" href="<?= site_url('auth') ?>">
+          <i class="fas fa-sign-in-alt"></i> Login
+        </a>
+      <?php endif; ?>
     </div>
   </div>
+  </div>
+  <!-- /.container -->
 </nav>  
+
+<div class="container kEVnV">
+  <h1 class="QZxoZ">
+    Layanan Aspirasi dan Pengaduan Online Rakyat
+  </h1>
+  <h1 class="qyzJO">
+    Sampaikan laporan Anda langsung kepada 
+    instansi pemerintah berwenang
+  </h1>
+  <hr class="mt-4 bQDxD">
+  <div class="pb-5"></div>
+</div>
   
 </section>
