6649c526b0ca561696e03b3d18c45b41094ba1dd
novaardiansyah78 <novaardiansyah817@gmail.com>
2020-03-20 23:55:58 +0700

    proses membuat fitur registrasi dengan modal dan ajax, tahap selanjutnya adalah validasi

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -60,7 +60,7 @@
   
 </section>
 
-<!-- Modal -->
+<!-- Modal Login -->
 <div class="modal fade" id="modal_login" data-backdrop="static" 
 tabindex="-1" role="dialog" aria-labelledby="modal_loginLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg" role="document">
@@ -77,8 +77,10 @@ tabindex="-1" role="dialog" aria-labelledby="modal_loginLabel" aria-hidden="true
         <p><i class="fas fa-check"></i> untuk dapat menggunakan layanan kami, 
         anda diharuskan login terlebih dahulu!</p>
         <form action="<?= site_url('auth') ?>" id="form_login">
+          
+          <!-- Username --> 
           <div class="input-group mb-3 mt-4">
-            <input type="text" class="form-control" id="username" 
+            <input type="text" class="form-control" id="l_username"
             placeholder="Username" name="username">
             <div class="input-group-append">
               <div class="input-group-text">
@@ -86,19 +88,20 @@ tabindex="-1" role="dialog" aria-labelledby="modal_loginLabel" aria-hidden="true
               </div>
             </div>
           </div>
-
+          
+          <!-- Password -->
           <div class="input-group mb-4">
-            <input type="password" class="form-control" id="password" 
+            <input type="password" class="form-control" id="l_password" 
             placeholder="Password" name="password">
             <div class="input-group-append">
               <div class="input-group-text">
-                <i class="fas fa-eye" id="show_hide" data-check="1"></i>
+                <i class="fas fa-eye" id="l_show" data-check="1"></i>
               </div>
             </div>
           </div>
           
           <div class="icheck-primary">
-            <input type="checkbox" id="terms" name="terms" value="">
+            <input type="checkbox" id="terms" value="">
             <label for="terms">
              <small> Saya telah membaca dan menyetujui
              <a href="#">Syarat dan Ketentuan Layanan</a> </small>
@@ -112,7 +115,7 @@ tabindex="-1" role="dialog" aria-labelledby="modal_loginLabel" aria-hidden="true
           </div>
           <hr>
           <div class="text-center">
-            <small>belum punya akun? <a href="">daftar</a> sekarang!</small>
+            <small>belum punya akun? <a href="" id="daftar">daftar</a> sekarang!</small>
           </div>
         </form>
       </div>
@@ -121,5 +124,111 @@ tabindex="-1" role="dialog" aria-labelledby="modal_loginLabel" aria-hidden="true
   </div>
 </div>
 
-
+<!-- Modal Register -->
+<div class="modal fade" id="modal_register" data-backdrop="static" 
+tabindex="-1" role="dialog" aria-labelledby="modal_registerLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header bg-danger">
+        <h5 class="modal-title" id="modal_registerLabel">
+          Register Masyarakat
+        </h5>
+        <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-dark">
+        <p><i class="fas fa-check"></i> untuk dapat mengirim laporan, anda harus 
+        menjadi anggota yang terdaftar dilayanan kami!</p>
+        <form action="<?= site_url('auth/register') ?>" id="form_register">
+          
+          <!-- Nama Lengkap -->
+          <div class="input-group mb-3 mt-4">
+            <input type="text" class="form-control" id="r_nama" 
+            placeholder="Nama Lengkap" name="nama">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <i class="fas fa-user"></i>
+              </div>
+            </div>
+          </div>
+            
+          <!-- Username -->
+          <div class="input-group mb-3 mt-4">
+            <input type="text" class="form-control" id="r_username"
+            placeholder="Username" name="username">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <i class="fas fa-user-lock"></i>
+              </div>
+            </div>
+          </div>
+            
+          <!-- Email -->
+          <div class="input-group mb-3 mt-4">
+            <input type="text" class="form-control" id="r_email" 
+            placeholder="Email" name="email">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <i class="fas fa-envelope"></i>
+              </div>
+            </div>
+          </div>
+          
+          <!-- No. Telepon -->
+          <div class="input-group mb-3 mt-4">
+            <input type="text" class="form-control" id="r_no_telp" 
+            placeholder="No. Telepon" name="no_telp">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <i class="fas fa-phone"></i>
+              </div>
+            </div>
+          </div>
+          
+          <!-- Password -->  
+          <div class="input-group mb-4">
+            <input type="password" class="form-control" id="r_password"
+            placeholder="Password" name="password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <i class="fas fa-lock" data-check="1"></i>
+              </div>
+            </div>
+          </div>
+          
+          <!-- Konfirmasi Password -->  
+          <div class="input-group mb-4">
+            <input type="password" class="form-control" id="r_password1"
+            placeholder="Konfirmasi Password" name="password1">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <i class="fas fa-eye" id="r_show" data-check="1"></i>
+              </div>
+            </div>
+          </div>
+          
+          <div class="icheck-primary">
+            <input type="checkbox" id="terms1" value="">
+            <label for="terms1">
+             <small> Saya telah membaca dan menyetujui
+             <a href="#">Syarat dan Ketentuan Layanan</a> </small>
+            </label>
+          </div>
+          
+          <div class="mt-4">
+            <button type="button" class="btn btn-danger btn-block tombol_register">
+              <i class="fas fa-paper-plane"></i> Register
+            </button>
+          </div>
+          <hr>
+          <div class="text-center">
+            <small>sudah punya akun? <a href="" id="login">login</a> sekarang!</small>
+          </div>
+        </form>
+      </div>
+      <!-- /.card-body -->
+    </div>
+  </div>
+</div>
 
--- assets/owner/js/home/index.js
+++ assets/owner/js/home/index.js
@@ -1,88 +1,42 @@
 $(document).ready(function(){
-  
-  // show / hide password
-  $('#show_hide').click(function(){
+/*========================================================================================================*/  
+  // show / hide password login
+  $('#l_show').click(function(){
     let check = $(this).data('check');
     if(check == 1)
     {
       $(this).removeClass('fa-eye');
       $(this).addClass('fa-eye-slash');
-      $('#password').attr('type','text');
+      $('#l_password').attr('type','text');
       check = $(this).data('check','0');
     } else {
       $(this).removeClass('fa-eye-slash');
       $(this).addClass('fa-eye');
-      $('#password').attr('type','password');
+      $('#l_password').attr('type','password');
       check = $(this).data('check','1');
     }
   });
   
-  // username 
-  $('#username').keyup(function(){
-    let username = $(this).val().length;
-    
-    if(username < 9 || username > 30)
+  // show / hide password register
+  $('#r_show').click(function(){
+    let check = $(this).data('check');
+    if(check == 1)
     {
-      $(this).removeClass('is-valid');
-      $(this).addClass('is-invalid');
+      $(this).removeClass('fa-eye');
+      $(this).addClass('fa-eye-slash');
+      $('#r_password, #r_password1').attr('type','text');
+      check = $(this).data('check','0');
     } else {
-      $(this).removeClass('is-invalid');
-      $(this).addClass('is-valid');
+      $(this).removeClass('fa-eye-slash');
+      $(this).addClass('fa-eye');
+      $('#r_password, #r_password1').attr('type','password');
+      check = $(this).data('check','1');
     }
   });
-  
-  // password
-  $('#password').keyup(function(){
-    let password = $(this).val().length;
-    
-    if(password < 9 || password > 30)
-    {
-      $(this).removeClass('is-valid');
-      $(this).addClass('is-invalid');
-    } else {
-      $(this).removeClass('is-invalid');
-      $(this).addClass('is-valid');
-    }
-  });
-  
-  $('.btn_kirim_laporan').click(function(e){
-    e.preventDefault();
-    let user = $(this).data('user');
-    if(user)
-    {
-      
-    } else {
-      $('#modal_login').modal('show');
-    }
-  });
-  
-  // cek terms sudah diceklis belum
-  $('.tombol_login').attr('disabled','true');
-  $('#terms').click(function(){
-    let check = $('#terms').attr('value');
-    if(check == '')
-    {
-      $('.tombol_login').removeAttr('disabled');
-      $('#terms').val(1);
-    } else {
-      $('.tombol_login').attr('disabled','true');
-      $('#terms').val('');
-    }
-  });
-  
-  // cek jika user akses profile
-  $('.nav-link.profile').click(function(e){
-    e.preventDefault();
-    let user = $('.btn_kirim_laporan').data('user');
-    let url = $(this).attr('href');
-    if(user)
-    {
-      $(location).attr('href',url);
-    } else {
-      $('#modal_login').modal('show');
-    }
-  });
-  
+/*========================================================================================================*/  
+
+
+/*========================================================================================================*/  
   // login dengan modal
   $('.tombol_login').click(function(){
     let data = $('#form_login').serialize();
@@ -122,6 +76,176 @@ $(document).ready(function(){
       }
     });
   });
-   
+  
+  // link login 
+  $('#login').click(function(e){
+    e.preventDefault();
+    $('#modal_register').modal('hide');
+    $('#modal_login').modal('show');
+  });
+  
+  // validasi login
+  
+  // username 
+  $('#l_username').keyup(function(){
+    let username = $(this).val().length;
+    
+    if(username < 9 || username > 30)
+    {
+      $(this).removeClass('is-valid');
+      $(this).addClass('is-invalid');
+    } else {
+      $(this).removeClass('is-invalid');
+      $(this).addClass('is-valid');
+    }
+  });
+  
+  // password
+  $('#l_password').keyup(function(){
+    let password = $(this).val().length;
+    
+    if(password < 9 || password > 30)
+    {
+      $(this).removeClass('is-valid');
+      $(this).addClass('is-invalid');
+    } else {
+      $(this).removeClass('is-invalid');
+      $(this).addClass('is-valid');
+    }
+  });
+  
+  // cek terms login
+  $('.tombol_login').attr('disabled','true');
+  $('#terms').click(function(){
+    let check = $('#terms').attr('value');
+    if(check == '')
+    {
+      $('.tombol_login').removeAttr('disabled');
+      $('#terms').val(1);
+    } else {
+      $('.tombol_login').attr('disabled','true');
+      $('#terms').val('');
+    }
+  });
+/*========================================================================================================*/  
 
+
+/*========================================================================================================*/
+  // link daftar
+  $('#daftar').click(function(e){
+    e.preventDefault();
+    $('#modal_login').modal('hide');
+    $('#modal_register').modal('show');
+  });
+  
+  // validasi registrasi
+  
+  // nama 
+  $('#r_nama').keyup(function(){
+    let nama = $(this).val().length;
+    
+    if(nama < 3 || nama > 120)
+    {
+      $(this).removeClass('is-valid');
+      $(this).addClass('is-invalid');
+    } else {
+      $(this).removeClass('is-invalid');
+      $(this).addClass('is-valid');
+    }
+  });
+  
+  // username 
+  $('#r_username').keyup(function(){
+    let username = $(this).val().length;
+    
+    if(username < 9 || username > 30)
+    {
+      $(this).removeClass('is-valid');
+      $(this).addClass('is-invalid');
+    } else {
+      $(this).removeClass('is-invalid');
+      $(this).addClass('is-valid');
+    }
+  });
+  
+  // password
+  $('#r_password').keyup(function(){
+    let password  = $(this).val().length;
+    let pass      = $(this).val();
+    let password1 = $('#r_password1').val();
+    
+    if(password1 != pass)
+    {
+      $('#r_password1').removeClass('is-valid');
+      $('#r_password1').addClass('is-invalid');
+    } else {
+      $('#r_password1').removeClass('is-invalid');
+      $('#r_password1').addClass('is-valid');
+    }
+    
+    if(password < 9 || password > 30)
+    {
+      $(this).removeClass('is-valid');
+      $(this).addClass('is-invalid');
+    } else {
+      $(this).removeClass('is-invalid');
+      $(this).addClass('is-valid');
+    }
+  });
+  
+  // konfirmasi password
+  $('#r_password1').keyup(function(){
+    let password = $('#r_password').val();
+    let password1 = $(this).val();
+    
+    if(password1 != password)
+    {
+      $(this).removeClass('is-valid');
+      $(this).addClass('is-invalid');
+    } else {
+      $(this).removeClass('is-invalid');
+      $(this).addClass('is-valid');
+    }
+  });
+  
+  // cek terms1 register
+  $('.tombol_register').attr('disabled','true');
+  $('#terms1').click(function(){
+    let check = $('#terms1').attr('value');
+    if(check == '')
+    {
+      $('.tombol_register').removeAttr('disabled');
+      $('#terms1').val(1);
+    } else {
+      $('.tombol_register').attr('disabled','true');
+      $('#terms1').val('');
+    }
+  });
+/*========================================================================================================*/
+
+  // tombol kirim laporan
+  $('.btn_kirim_laporan').click(function(e){
+    e.preventDefault();
+    let user = $(this).data('user');
+    if(user)
+    {
+      
+    } else {
+      $('#modal_login').modal('show');
+    }
+  });
+  
+  // cek jika user akses profile
+  $('.nav-link.profile').click(function(e){
+    e.preventDefault();
+    let user = $('.btn_kirim_laporan').data('user');
+    let url = $(this).attr('href');
+    if(user)
+    {
+      $(location).attr('href',url);
+    } else {
+      $('#modal_login').modal('show');
+    }
+  });
+  
 });
