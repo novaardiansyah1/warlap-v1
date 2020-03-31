7169af14ac11603acf000a6ca4023fdf0e1d64c5
novaardiansyah1 <novaardiansyah817@gmail.com>
2020-03-31 16:57:30 +0700

    memperbaiki error ketika proses update submenu

--- application/models/M_submenu.php
+++ application/models/M_submenu.php
@@ -70,32 +70,49 @@ class M_submenu extends CI_Model
       echo('true');
     }
   }
-  
+
+/*=============================
+  update submenu
+  =============================*/
   function update()
   {
-    $submenu = $this->is_up_submenu();
+    $id        = htmlspecialchars($_POST['id']);
+    $submenu   = htmlspecialchars($_POST['submenu']);
+    $menu_id   = htmlspecialchars($_POST['menu_id']);
+    $link      = htmlspecialchars($_POST['link']);
+    $icon      = htmlspecialchars($_POST['icon']);
+    $is_active = htmlspecialchars($_POST['is_active']);
     
-    if($submenu == 'true')
-    {
-      $id        = htmlspecialchars($_POST['id']);
-      $submenu   = htmlspecialchars($_POST['submenu']);
-      $menu_id   = htmlspecialchars($_POST['menu_id']);
-      $link      = htmlspecialchars($_POST['link']);
-      $icon      = htmlspecialchars($_POST['icon']);
-      $is_active = htmlspecialchars($_POST['is_active']);
-      
-      $this->db->update('submenu',[
-        'submenu'   => $submenu,
-        'menu_id'   => $menu_id,
-        'link'      => $link,
-        'icon'      => $icon,
-        'is_active' => $is_active,
-      ], ['id' => $id]);
-        
-      echo 'true';
+    $sub1    = $this->db->get_where('submenu', ['submenu' => $submenu])->row_array();
+    $sub2    = $this->db->get_where('submenu', ['id' => $id])->row_array();
+    
+    if($sub1 !== null) {
+      if($sub1['submenu'] !== $sub2['submenu']) {
+        echo('false');
+      } else {
+        $this->proses_update($id,$submenu,$menu_id,$link,$icon,$is_active);
+      }
+    } else {
+      $this->proses_update($id,$submenu,$menu_id,$link,$icon,$is_active);
     }
   }
 
+/*=============================
+  proses update submenu
+  =============================*/
+  function proses_update($id,$submenu,$menu_id,$link,$icon,$is_active)
+  {
+    $this->db->update('submenu',[
+      'submenu'   => $submenu,
+      'menu_id'   => $menu_id,
+      'link'      => $link,
+      'icon'      => $icon,
+      'is_active' => $is_active,
+    ], ['id' => $id]);
+    
+    echo('true');
+  }
+  
   function delete($id)
   {
     $this->db->delete('submenu', ['id' => $id]);
--- assets/owner/js/submenu/index.js
+++ assets/owner/js/submenu/index.js
@@ -1,231 +1,243 @@
 $(document).ready(function() {
 
-let url = base_url('submenu/');
+/*=============================
+  base_url
+  =============================*/
+  let url = base_url('submenu/');
 
-$('#btn-cr_submenu').click(function(){
-  $('#modal-cr_submenu').modal('show');
-});
+/*=============================
+  tampilkan modal create
+  =============================*/
+  $('#btn-cr_submenu').click(function(){
+    $('#modal-cr_submenu').modal('show');
+  });
 
-$('#cr-submenu').keyup(function(){
-  let submenu = $(this).val();
-  let href    = url + 'is_submenu';
-  let data    = $('#form_create').serialize();
-  
-  $.ajax({
-    url: href,
-    type: 'post',
-    data: data,
-    success: function(result) {
-      if(result > 0) {
-        invalid('#cr-submenu');
-      } else {
-        if (submenu.length < 3 || submenu.length > 20) {
+  $('#cr-submenu').keyup(function(){
+    let submenu = $(this).val();
+    let href    = url + 'is_submenu';
+    let data    = $('#form_create').serialize();
+    
+    $.ajax({
+      url: href,
+      type: 'post',
+      data: data,
+      success: function(result) {
+        if(result > 0) {
           invalid('#cr-submenu');
         } else {
-          valid('#cr-submenu');
+          if (submenu.length < 3 || submenu.length > 20) {
+            invalid('#cr-submenu');
+          } else {
+            valid('#cr-submenu');
+          }
         }
       }
+    });
+  });
+
+  $('#cr-menu_id').change(function(){
+    let menu = $(this).val();
+    if(menu.length < 1){
+      invalid('#cr-menu_id');
+    } else {
+      valid('#cr-menu_id');
     }
   });
-});
 
-$('#cr-menu_id').change(function(){
-  let menu = $(this).val();
-  if(menu.length < 1){
-    invalid('#cr-menu_id');
-  } else {
-    valid('#cr-menu_id');
-  }
-});
-
-$('#cr-link').keyup(function(){
-  let link = $(this).val();
-  let href = url + 'is_link';
-  let data = $('#form_create').serialize();
-  
-  $.ajax({
-    url: href,
-    type: 'post',
-    data: data,
-    success: function(result) {
-      if(result > 0) {
-        invalid('#cr-link');
-      } else {
-        if (link.length < 3 || link.length > 120) {
+  $('#cr-link').keyup(function(){
+    let link = $(this).val();
+    let href = url + 'is_link';
+    let data = $('#form_create').serialize();
+    
+    $.ajax({
+      url: href,
+      type: 'post',
+      data: data,
+      success: function(result) {
+        if(result > 0) {
           invalid('#cr-link');
         } else {
-          valid('#cr-link');
+          if (link.length < 3 || link.length > 120) {
+            invalid('#cr-link');
+          } else {
+            valid('#cr-link');
+          }
         }
       }
+    });
+  });
+
+  $('#cr-icon').keyup(function(){
+    let icon = $(this).val();
+    
+    if(icon.length < 8 || icon.length > 30) {
+      invalid('#cr-icon');
+    } else {
+      valid('#cr-icon');
     }
   });
-});
 
-$('#cr-icon').keyup(function(){
-  let icon = $(this).val();
-  
-  if(icon.length < 8 || icon.length > 30) {
-    invalid('#cr-icon');
-  } else {
-    valid('#cr-icon');
-  }
-});
-
-$('#submit-cr_submenu').click(function(){
-  let href = url + 'create';
-  let data = $('#form_create').serialize();
-  let tombol = $(this).html();
-  
-  $.ajax({
-    url: href,
-    data: data,
-    type: 'post',
-    beforeSend: function() {
-      loading('#submit-cr_submenu');
-    },
-    success: function(result) {
-      if(result == 'true') {
-        Swal.fire({
-          title: '',
-          html: 'submenu berhasil ditambahkan.',
-          type: 'success'
-        });
+  $('#submit-cr_submenu').click(function(){
+    let href = url + 'create';
+    let data = $('#form_create').serialize();
+    let tombol = $(this).html();
+    
+    $.ajax({
+      url: href,
+      data: data,
+      type: 'post',
+      beforeSend: function() {
+        loading('#submit-cr_submenu');
+      },
+      success: function(result) {
+        if(result == 'true') {
+          Swal.fire({
+            title: '',
+            html: 'submenu berhasil ditambahkan.',
+            type: 'success'
+          });
+          
+          setTimeout(function() {
+            $(location).attr('href', href);
+          }, 2000);
+        } else {
+          Swal.fire({
+            title: '',
+            html: 'gagal menambahkan submenu, mohon cek kembali formulir anda.',
+            type: 'error'
+          });
+        }
         
-        setTimeout(function() {
-          $(location).attr('href', href);
-        }, 2000);
-      } else {
-        Swal.fire({
-          title: '',
-          html: 'gagal menambahkan submenu, mohon cek kembali formulir anda.',
-          type: 'error'
-        });
+        loading('#submit-cr_submenu', tombol);
       }
-      
-      loading('#submit-cr_submenu', tombol);
-    }
+    });
+    
   });
-  
-});
 
-$('.btn-up_submenu').click(function(e) {
-  e.preventDefault();
-  let href = $(this).attr('href');
+  $('.btn-up_submenu').click(function(e) {
+    e.preventDefault();
+    let href = $(this).attr('href');
+    
+    $.ajax({
+      url: href,
+      data: null,
+      type: 'post',
+      success: function(result) {
+        let data = JSON.parse(result);
+        $('#up-id').val(data.id);
+        $('#up-submenu').val(data.submenu);
+        $('#up-link').val(data.link);
+        $('#up-icon').val(data.icon);
+        $('#up-menu_id').val(data.menu_id);
+        $('#up-is_active').val(data.is_active);
+      }
+    });
   
-  $.ajax({
-    url: href,
-    data: null,
-    type: 'post',
-    success: function(result) {
-      let data = JSON.parse(result);
-      $('#up-id').val(data.id);
-      $('#up-submenu').val(data.submenu);
-      $('#up-link').val(data.link);
-      $('#up-icon').val(data.icon);
-      $('#up-menu_id').val(data.menu_id);
-      $('#up-is_active').val(data.is_active);
-    }
+    $('#modal-up_submenu').modal('show');
   });
-  
-  $('#modal-up_submenu').modal('show');
-});
 
-$('#up-submenu').keyup(function() {
-  let id      = $('#up-id').val();
-  let submenu = $(this).val();
-  let href    = url + 'is_up_submenu';
-  
-  $.ajax({
-    url: href,
-    data: {
-      id: id,
-      submenu: submenu
-    },
-    type: 'post',
-    success: function(result) {
-      if(result == 'false') {
-        invalid('#up-submenu');
-      } else {
-        if (submenu.length < 3 || submenu.length > 20) {
+  $('#up-submenu').keyup(function() {
+    let id      = $('#up-id').val();
+    let submenu = $(this).val();
+    let href    = url + 'is_up_submenu';
+    
+    $.ajax({
+      url: href,
+      data: {
+        id: id,
+        submenu: submenu
+      },
+      type: 'post',
+      success: function(result) {
+        if(result == 'false') {
           invalid('#up-submenu');
         } else {
-          valid('#up-submenu');
+          if (submenu.length < 3 || submenu.length > 20) {
+            invalid('#up-submenu');
+          } else {
+            valid('#up-submenu');
+          }
         }
       }
+    });
+  });
+  
+  $('#up-menu_id').change(function(){
+    let menu = $(this).val();
+    if(menu.length < 1){
+      invalid('#up-menu_id');
+    } else {
+      valid('#up-menu_id');
     }
   });
-});
-
-$('#up-menu_id').change(function(){
-  let menu = $(this).val();
-  if(menu.length < 1){
-    invalid('#up-menu_id');
-  } else {
-    valid('#up-menu_id');
-  }
-});
-
-$('#up-link').keyup(function(){
-  let link = $(this).val();
   
-  if (link.length < 3 || link.length > 120) {
-    invalid('#up-link');
-  } else {
-    valid('#up-link');
-  }
-});
-
-$('#up-icon').keyup(function(){
-  let icon = $(this).val();
+  $('#up-link').keyup(function(){
+    let link = $(this).val();
+    
+    if (link.length < 3 || link.length > 120) {
+      invalid('#up-link');
+    } else {
+      valid('#up-link');
+    }
+  });
   
-  if(icon.length < 8 || icon.length > 30) {
-    invalid('#up-icon');
-  } else {
-    valid('#up-icon');
-  }
-});
+  $('#up-icon').keyup(function(){
+    let icon = $(this).val();
+    
+    if(icon.length < 8 || icon.length > 30) {
+      invalid('#up-icon');
+    } else {
+      valid('#up-icon');
+    }
+  });
 
-$('#submit-up_submenu').click(function(){
-  let href   = url + 'update';
-  let data   = $('#form-up_submenu').serialize();
-  let tombol = $(this).html();
-  
-  $.ajax({
-    url: href,
-    data: data,
-    type: 'post',
-    beforeSend: function() {
-      loading('#submit-up_submenu');
-    },
-    success: function(result) {
-      if(result == 'true') {
-        Swal.fire({
-          title: '',
-          html: 'submenu berhasil diperbarui.',
-          type: 'success'
-        });
+/*=============================
+  proses update submenu
+  =============================*/
+  $('#submit-up_submenu').click(function(){
+    let href   = url + 'update';
+    let data   = $('#form-up_submenu').serialize();
+    let tombol = $(this).html();
+    
+    $.ajax({
+      url: href,
+      data: data,
+      type: 'post',
+      beforeSend: function() {
+        loading('#submit-up_submenu');
+      },
+      success: function(result) {
+        if(result == 'true') {
+          Swal.fire({
+            title: '',
+            html: 'submenu berhasil diperbarui.',
+            type: 'success'
+          });
+          
+          setTimeout(function() {
+            $(location).attr('href', href);
+          }, 2000);
+        } else {
+          Swal.fire({
+            title: '',
+            html: 'gagal memperbarui submenu, mohon cek kembali formulir anda.',
+            type: 'error'
+          });
+        }
         
-        setTimeout(function() {
-          $(location).attr('href', href);
-        }, 2000);
-      } else {
-        Swal.fire({
-          title: '',
-          html: 'gagal memperbarui submenu, mohon cek kembali formulir anda.',
-          type: 'error'
-        });
+        loading('#submit-up_submenu', tombol);
       }
-      
-      loading('#submit-up_submenu', tombol);
-    }
+    });
   });
-});
-
-$('.btn_delete').click(function(e) {
-  e.preventDefault();
-  let href = $(this).attr('href');
   
-  confirm_delete(href);
-});
+/*=============================
+  proses hapus submenu
+  =============================*/
+  $('.btn_delete').click(function(e) {
+    e.preventDefault();
+    let href = $(this).attr('href');
+    
+    confirm_delete(href);
+  });
 
 });
